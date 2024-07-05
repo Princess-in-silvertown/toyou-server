6cdf7c9ab4bcf54ebbbe866200380725731e8ae8
Hyoseop Song <crad_on25@naver.com>
2024-07-06 00:54:29 +0900

    :zap: Feat: 응답 성공/실패 타입 HttpStatusCode 필드 추가 (#6)

--- src/main/java/slvtwn/khu/toyouserver/common/ErrorType.java
+++ src/main/java/slvtwn/khu/toyouserver/common/ErrorType.java
@@ -1,17 +1,42 @@
 package slvtwn.khu.toyouserver.common;
 
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 
-@Getter
-@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public enum ErrorType {
 
-	// 404 (Not Found)
-	NOT_FOUND("TYU404", "Not Found");
+	// 400 ~ 499 (요청 오류)
+	RESPONSE_FORMAT_ERROR(HttpStatus.BAD_REQUEST, "TYU-400", "응답 형식 오류"),
+	BAD_REQUEST(HttpStatus.BAD_REQUEST, "TYU-400", "잘못된 요청입니다."),
+	NOT_FOUND(HttpStatus.NOT_FOUND, "TYU-404", "요청한 자원을 찾을 수 없습니다."),
 
+	// 500 ~ 599 (서버 오류)
+	INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "TYU-500", "서버 내부 오류");
+
+	ErrorType(
+			final HttpStatusCode httpStatusCode,
+			final String code,
+			final String message
+	) {
+		this.httpStatusCode = httpStatusCode;
+		this.code = code;
+		this.message = message;
+	}
+
+	private final HttpStatusCode httpStatusCode;
 	private final String code;
-
 	private final String message;
+
+	public HttpStatusCode httpStatusCode() {
+		return httpStatusCode;
+	}
+
+	public String code() {
+		return code;
+	}
+
+	public String message() {
+		return message;
+	}
+
 }
--- src/main/java/slvtwn/khu/toyouserver/common/SuccessType.java
+++ src/main/java/slvtwn/khu/toyouserver/common/SuccessType.java
@@ -3,14 +3,17 @@ package slvtwn.khu.toyouserver.common;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 
 @Getter
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public enum SuccessType {
 
 	// 200 (OK)
-	OK("TYU200", "success");
+	OK(HttpStatus.OK, "TYU-200", "응답 성공");
 
+	private final HttpStatusCode httpStatusCode;
 	private final String code;
 	private final String message;
 }
