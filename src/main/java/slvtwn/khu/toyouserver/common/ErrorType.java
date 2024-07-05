a8d44f00be75737261c087cb21fe7c5ef5c83521
Hyoseop Song <crad_on25@naver.com>
2024-07-06 07:06:42 +0900

    :sparkles: Feat : 그룹 저장 Repository Layer 구현 (#13)

--- src/main/java/slvtwn/khu/toyouserver/common/ErrorType.java
+++ src/main/java/slvtwn/khu/toyouserver/common/ErrorType.java
@@ -9,6 +9,7 @@ public enum ErrorType {
 	RESPONSE_FORMAT_ERROR(HttpStatus.BAD_REQUEST, "TYU-400", "응답 형식 오류"),
 	BAD_REQUEST(HttpStatus.BAD_REQUEST, "TYU-400", "잘못된 요청입니다."),
 	NOT_FOUND(HttpStatus.NOT_FOUND, "TYU-404", "요청한 자원을 찾을 수 없습니다."),
+	GROUP_NOT_FOUND(HttpStatus.NOT_FOUND, "TYU-4041", "그룹을 찾을 수 없습니다."),
 
 	// 500 ~ 599 (서버 오류)
 	INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "TYU-500", "서버 내부 오류");
