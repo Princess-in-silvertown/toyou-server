168681ee6236c598d2a17141e0b57c17503a8a1f
Hyoseop Song <crad_on25@naver.com>
2024-07-12 15:57:15 +0900

    :sparkles: feat: 그룹 생성 API presentation (#23)

--- src/main/java/slvtwn/khu/toyouserver/presentation/GroupController.java
+++ src/main/java/slvtwn/khu/toyouserver/presentation/GroupController.java
@@ -3,18 +3,25 @@ package slvtwn.khu.toyouserver.presentation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import slvtwn.khu.toyouserver.application.GroupService;
+import slvtwn.khu.toyouserver.dto.GroupCreateRequest;
 import slvtwn.khu.toyouserver.dto.GroupResponse;
 
 @RequiredArgsConstructor
 @RestController
 public class GroupController {
 
-    private final GroupService groupService;
+	private final GroupService groupService;
 
-    @PostMapping("/groups/{groupId}/members")
-    public GroupResponse registerMember(@PathVariable long groupId) {
-        return groupService.registerUser(groupId, 1L); // TODO: user -> argumentResolver 등록 필요
-    }
+	@PostMapping("/groups/create")
+	public GroupResponse createGroup(@RequestBody GroupCreateRequest request) {
+		return groupService.create(request.name());
+	}
+
+	@PostMapping("/groups/{groupId}/members")
+	public GroupResponse registerMember(@PathVariable long groupId) {
+		return groupService.registerUser(groupId, 1L); // TODO: user -> argumentResolver 등록 필요
+	}
 }
