4cecf191c82c298a01b37f8a85002c7d0346b1b6
Hyoseop Song <crad_on25@naver.com>
2024-07-13 18:46:48 +0900

    :sparkles: feat: 유저 세션 인증 ArgumentResolver 구현 (#21)

---
+++ src/main/java/slvtwn/khu/toyouserver/common/WebConfig.java
@@ -0,0 +1,18 @@
+package slvtwn.khu.toyouserver.common;
+
+import jakarta.servlet.http.HttpSession;
+import java.util.List;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebConfig implements WebMvcConfigurer {
+
+	private HttpSession httpSession;
+
+	private SessionUserService sessionUserService;
+
+	@Override
+	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+		resolvers.add(new CookieAuthArgumentResolver(httpSession, sessionUserService));
+	}
+}
