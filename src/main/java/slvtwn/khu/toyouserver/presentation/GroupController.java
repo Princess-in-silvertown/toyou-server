67f8882ddc1b8af0ac57961ddc2992cd1207c898
Hyoseop Song <crad_on25@naver.com>
2024-07-13 19:16:11 +0900

    :fire: fix: 그룹 생성 API 조회 endpoint 수정 (#17) - POST /groups/create -> POST /groups

--- src/main/java/slvtwn/khu/toyouserver/presentation/GroupController.java
+++ src/main/java/slvtwn/khu/toyouserver/presentation/GroupController.java
@@ -1,8 +1,6 @@
 package slvtwn.khu.toyouserver.presentation;
 
-import java.util.List;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,11 +15,11 @@ public class GroupController {
 
 	private final GroupService groupService;
 
-	@PostMapping("/groups/create")
+	@PostMapping("/groups")
 	public GroupResponse createGroup(@RequestBody GroupCreateRequest request) {
 		return groupService.create(request.name());
 	}
-  
+
 	@PostMapping("/groups/{groupId}/members")
 	public GroupResponse registerMember(@PathVariable long groupId) {
 		return groupService.registerUser(groupId, 1L); // TODO: user -> argumentResolver 등록 필요
