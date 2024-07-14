b5367d676f52090057ed375802e90691c463dcda
Hyoseop Song <crad_on25@naver.com>
2024-07-14 18:11:42 +0900

    :sparkles: feat: 가입한 유저 조회API Presentation (#17)

--- src/main/java/slvtwn/khu/toyouserver/presentation/GroupController.java
+++ src/main/java/slvtwn/khu/toyouserver/presentation/GroupController.java
@@ -1,11 +1,15 @@
 package slvtwn.khu.toyouserver.presentation;
 
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import slvtwn.khu.toyouserver.application.GroupService;
+import slvtwn.khu.toyouserver.common.UserAuthentication;
+import slvtwn.khu.toyouserver.domain.User;
 import slvtwn.khu.toyouserver.dto.GroupCreateRequest;
 import slvtwn.khu.toyouserver.dto.GroupResponse;
 
@@ -23,5 +27,10 @@ public class GroupController {
 	@PostMapping("/groups/{groupId}/members")
 	public GroupResponse registerMember(@PathVariable long groupId) {
 		return groupService.registerUser(groupId, 1L); // TODO: user -> argumentResolver 등록 필요
+	}
+
+	@GetMapping("/groups")
+	public List<GroupResponse> findRegisteredGroups(@UserAuthentication User user) {
+		return groupService.findRegisteredGroups(user.getId());
 	}
 }
