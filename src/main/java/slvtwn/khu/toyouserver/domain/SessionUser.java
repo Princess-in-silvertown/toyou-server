a2da75efc56af7f44341799ddf737780038e705e
Hyoseop Song <crad_on25@naver.com>
2024-07-14 17:37:33 +0900

    :fire: fix: 유저 세션 테이블명 수정 (#17) session_user -> session_users

--- src/main/java/slvtwn/khu/toyouserver/domain/SessionUser.java
+++ src/main/java/slvtwn/khu/toyouserver/domain/SessionUser.java
@@ -3,12 +3,14 @@ package slvtwn.khu.toyouserver.domain;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import slvtwn.khu.toyouserver.common.BaseTimeEntity;
 
 @Entity
+@Table(name = "session_users")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 public class SessionUser extends BaseTimeEntity {
@@ -19,6 +21,8 @@ public class SessionUser extends BaseTimeEntity {
 
 	@Column(name = "user_id", nullable = false)
 	private Long userId;
+
+	// TODO ; 카드
 
 	public SessionUser(String sessionId, Long userId) {
 		this.sessionId = sessionId;
