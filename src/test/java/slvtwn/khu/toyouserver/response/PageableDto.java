1e30a73ea403189b80430b78573f5ed356e27ef9
woosung1223 <dntjd991223@naver.com>
2024-08-18 22:32:11 +0900

    :art: refactor: 불필요한 테스트 삭제

--- src/test/java/slvtwn/khu/toyouserver/response/PageableDto.java
+++
@@ -1,9 +0,0 @@
-package slvtwn.khu.toyouserver.response;
-
-import slvtwn.khu.toyouserver.common.PageInfoResponse;
-
-public record PageableDto(
-		Long id,
-		String name,
-		PageInfoResponse pageInfo) {
-}
