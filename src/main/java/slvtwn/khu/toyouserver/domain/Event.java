1a758e90c185d61f0d494f4545563c8ac4ffef49
woosung1223 <dntjd991223@naver.com>
2024-08-27 00:40:37 +0900

    :sparkles: feat: Event 객체 추가

---
+++ src/main/java/slvtwn/khu/toyouserver/domain/Event.java
@@ -0,0 +1,38 @@
+package slvtwn.khu.toyouserver.domain;
+
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.time.LocalDate;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Event {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private LocalDate date;
+
+    @Embedded
+    private EventType eventType;
+
+    private String description;
+
+    private String profileImageUrl;
+
+    public Event(String name, LocalDate date, EventType eventType, String description, String profileImageUrl) {
+        this.name = name;
+        this.date = date;
+        this.eventType = eventType;
+        this.description = description;
+        this.profileImageUrl = profileImageUrl;
+    }
+}
---
+++ src/main/java/slvtwn/khu/toyouserver/domain/EventType.java
@@ -0,0 +1,10 @@
+package slvtwn.khu.toyouserver.domain;
+
+import jakarta.persistence.Embeddable;
+
+@Embeddable
+public enum EventType {
+
+    BIRTHDAY,
+    ;
+}
