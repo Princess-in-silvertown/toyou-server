e41de714bcbc4837c6aff5a9563b63765218da5d
Hyoseop Song <crad_on25@naver.com>
2024-09-13 13:27:04 +0900

    :sparkles: feat: 롤링 페이퍼 전송 수신자 편지함 등록 로직 구현 (#41)

--- src/main/java/slvtwn/khu/toyouserver/application/RollingPaperService.java
+++ src/main/java/slvtwn/khu/toyouserver/application/RollingPaperService.java
@@ -21,28 +21,27 @@ import slvtwn.khu.toyouserver.persistance.StickerRepository;
 @Transactional(readOnly = true)
 public class RollingPaperService {
 
-    private final RollingPaperRepository rollingPaperRepository;
-    private final StickerRepository stickerRepository;
-    private final MemberRepository memberRepository;
+	private final RollingPaperRepository rollingPaperRepository;
+	private final StickerRepository stickerRepository;
+	private final MemberRepository memberRepository;
 
-    public void sendRollingPaper(RollingPaperRequest request, Long groupId, Long memberId) {
-        Member member = memberRepository.findById(memberId)
-                .orElseThrow(() -> new ToyouException(ResponseType.BAD_REQUEST));
+	public void sendRollingPaper(Long recipientUserId, RollingPaperRequest request) {
+		Member member = memberRepository.findByUserIdAndGroupId(recipientUserId, request.groupId()).orElseThrow(
+				() -> new ToyouException(ResponseType.BAD_REQUEST));
+		RollingPaper rollingPaper = new RollingPaper(request.coverImageUrl(), request.title(),
+				request.content(), request.themeId(), member);
+		List<Sticker> stickers = parseStickers(request, rollingPaper);
 
-        RollingPaper rollingPaper = new RollingPaper(request.coverImageUrl(), request.title(),
-                request.content(), request.themeId(), member);
-        List<Sticker> stickers = parseStickers(request, rollingPaper);
+		rollingPaperRepository.save(rollingPaper);
+		stickerRepository.saveAll(stickers);
+	}
 
-        rollingPaperRepository.save(rollingPaper);
-        stickerRepository.saveAll(stickers);
-    }
-
-    private List<Sticker> parseStickers(RollingPaperRequest request, RollingPaper rollingPaper) {
-        return request.stickers().stream()
-                .map(each -> new Sticker(rollingPaper, each.imageUrl(), each.x(), each.y(),
-                        each.rotate(), each.scale(), StickerSide.valueOf(each.side())))
-                .toList();
-    }
+	private List<Sticker> parseStickers(RollingPaperRequest request, RollingPaper rollingPaper) {
+		return request.stickers().stream()
+				.map(each -> new Sticker(rollingPaper, each.imageUrl(), each.x(), each.y(),
+						each.rotate(), each.scale(), StickerSide.valueOf(each.side())))
+				.toList();
+	}
 
     public RollingPaperResponse getRollingPaper(Long groupId, Long memberId, Long rollingPaperId) {
         // TODO: 검증 필요
