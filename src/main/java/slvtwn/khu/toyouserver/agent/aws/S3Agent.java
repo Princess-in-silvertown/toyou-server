dbabf453afb944ccef3a274360a16df6c94a79ab
woosung1223 <dntjd991223@naver.com>
2024-09-30 15:13:55 +0900

    :sparkles: feat: S3 저장 로직 생성

---
+++ src/main/java/slvtwn/khu/toyouserver/agent/aws/S3Agent.java
@@ -0,0 +1,35 @@
+package slvtwn.khu.toyouserver.agent.aws;
+
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import java.io.ByteArrayInputStream;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import slvtwn.khu.toyouserver.common.response.ResponseType;
+import slvtwn.khu.toyouserver.exception.ToyouException;
+
+@RequiredArgsConstructor
+@Component
+public class S3Agent {
+
+    private final AmazonS3Client amazonS3Client;
+    private final AwsConfiguration configuration;
+
+    public String uploadFile(byte[] fileData, String fileName, String contentType) {
+        try {
+            String fileUrl = "https://" + configuration.getBucket() + "/to-you/" + fileName;
+
+            ObjectMetadata metadata = new ObjectMetadata();
+            metadata.setContentType(contentType);
+            metadata.setContentLength(fileData.length);
+
+            ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(fileData);
+            amazonS3Client.putObject(new PutObjectRequest(configuration.getBucket(), fileName, byteArrayInputStream, metadata));
+
+            return fileUrl;
+        } catch (Exception e) {
+            throw new ToyouException(ResponseType.INTERNAL_SERVER_ERROR);
+        }
+    }
+}
