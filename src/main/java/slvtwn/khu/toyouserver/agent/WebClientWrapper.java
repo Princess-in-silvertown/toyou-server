42e0faf9c60a811e538adb62f62963e8ee797afb
woosung1223 <dntjd991223@naver.com>
2024-10-01 13:44:09 +0900

    :bug: fix: webClient 버퍼 메모리 사이즈 증량

--- src/main/java/slvtwn/khu/toyouserver/agent/WebClientWrapper.java
+++ src/main/java/slvtwn/khu/toyouserver/agent/WebClientWrapper.java
@@ -4,20 +4,28 @@ import java.util.function.Consumer;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.ExchangeStrategies;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Component
 public class WebClientWrapper {
 
     public <T> T send(String uri, Consumer<HttpHeaders> headersConsumer, Object body, Class<T> responseType) {
-        return WebClient.create()
-                .post()
+        ExchangeStrategies exchangeStrategies = ExchangeStrategies.builder()
+                .codecs(configurer -> configurer.defaultCodecs().maxInMemorySize(10 * 1024 * 1024))
+                .build();
+
+        WebClient webClient = WebClient.builder()
+                .exchangeStrategies(exchangeStrategies)
+                .build();
+
+        return webClient.post()
                 .uri(uri)
                 .header("Content-Type", "application/json")
                 .headers(headersConsumer)
                 .bodyValue(body)
                 .retrieve()
-                .onStatus(HttpStatusCode::is4xxClientError,
+                .onStatus(HttpStatusCode::isError,
                         clientResponse -> clientResponse.bodyToMono(String.class).map(Exception::new))
                 .bodyToMono(responseType)
                 .block();
