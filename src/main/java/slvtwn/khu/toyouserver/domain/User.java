2db55f638526eddafd00b4b9f0585aac44f6c350
woosung1223 <dntjd991223@naver.com>
2024-10-03 17:14:39 +0900

    :bug: fix: 중복 사용자를 반환하지 않도록 수정

--- src/main/java/slvtwn/khu/toyouserver/domain/User.java
+++ src/main/java/slvtwn/khu/toyouserver/domain/User.java
@@ -8,6 +8,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import java.time.LocalDate;
+import java.util.Objects;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -56,5 +57,18 @@ public class User extends BaseTimeEntity {
 		this.introduction = introduction;
 		this.profilePicture = profilePicture;
 		return this;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		User user = (User) o;
+		return Objects.equals(id, user.id) && Objects.equals(name, user.name);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, name);
 	}
 }
