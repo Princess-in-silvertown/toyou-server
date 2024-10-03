99cf7c0929fd27fee02e835518246c5a1201a837
woosung1223 <dntjd991223@naver.com>
2024-10-03 17:34:09 +0900

    :bug: fix: Member equals 재정의 (ID가 아닌 유저로 판단)

--- src/main/java/slvtwn/khu/toyouserver/domain/Member.java
+++ src/main/java/slvtwn/khu/toyouserver/domain/Member.java
@@ -39,11 +39,11 @@ public class Member extends BaseTimeEntity {
 		if (this == o) return true;
 		if (o == null || getClass() != o.getClass()) return false;
 		Member member = (Member) o;
-		return Objects.equals(id, member.id);
+		return Objects.equals(user, member.user);
 	}
 
 	@Override
 	public int hashCode() {
-		return Objects.hash(id);
+		return Objects.hash(user);
 	}
 }
